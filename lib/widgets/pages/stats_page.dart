0136811d488c768dddade20143402abc0b6b2f90
Omid Marfavi <midder@outlook.com>
2020-06-02 11:29:07 +0200

    Add nav bar & corresponding pages; Add nav logic

---
+++ lib/widgets/pages/stats_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StatsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
