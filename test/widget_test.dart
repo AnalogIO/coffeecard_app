41f10716bd509723a5fc50bccbc82854c89006ab
Omid Marfavi <midder@outlook.com>
2020-06-02 11:38:21 +0200

    Update test to change pages

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,16 +15,16 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that our app bar title reads 'Tickets'.
+    expect(find.text('Tickets'), findsOneWidget);
+    expect(find.text('Receipts'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the Receipts icon in the navigation bar and trigger a frame.
+    await tester.tap(find.byIcon(Icons.receipt));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that we have changed pages.
+    expect(find.text('Tickets'), findsNothing);
+    expect(find.text('Receipts'), findsOneWidget);
   });
 }
