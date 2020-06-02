ed75a4b2ac982e4d511e33d78319a85a7d407abd
Omid Marfavi <midder@outlook.com>
2020-06-02 11:21:55 +0200

    Set colors in MaterialApp; Add swatch generator

--- lib/base/style/colors.dart
+++ lib/base/style/colors.dart
@@ -9,8 +9,30 @@ abstract class AppColor {
   static const Color white = Color(0xffFAFAFA);
   static const Color gray = Color(0xff767676);
   static const Color lightGray = Color(0xffC2C2C2);
-  
+
   static const Color slightlyHighlighted = Color(0xffFEF4D2);
   static const Color success = Color(0xff4CAF50);
   static const Color error = Color(0xffFF4D00);
+
+  /// Creates a swatch from a given Color.
+  /// https://medium.com/@filipvk/creating-a-custom-color-swatch-in-flutter-554bcdcb27f3
+  static MaterialColor createMaterialColor(Color color) {
+    List strengths = <double>[.05];
+    Map swatch = <int, Color>{};
+    final int r = color.red, g = color.green, b = color.blue;
+
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+    strengths.forEach((strength) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    });
+    return MaterialColor(color.value, swatch);
+  }
 }
