4ab22ec00f77ba25084231b17b2dd00333392654
Omid Marfavi <midder@outlook.com>
2020-06-03 07:45:48 +0200

    Create Strings class

---
+++ lib/base/strings.dart
@@ -0,0 +1,8 @@
+abstract class Strings {
+  static const appTitle = 'Cafe Analog';
+
+  static const ticketsPageTitle = 'Tickets';
+  static const receiptsPageTitle = 'Receipts';
+  static const statsPageTitle = 'Statistics';
+  static const settingsPageTitle = 'Settings';
+}
