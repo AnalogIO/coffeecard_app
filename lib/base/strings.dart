b106c8142c8cf08275aaa8335a2c3ab8d19d7af7
Omid Marfavi <midder@outlook.com>
2020-06-03 07:46:49 +0200

    Add navigation bar titles in Strings

--- lib/base/strings.dart
+++ lib/base/strings.dart
@@ -1,8 +1,15 @@
 abstract class Strings {
   static const appTitle = 'Cafe Analog';
 
+  // Titles for the app bar.
   static const ticketsPageTitle = 'Tickets';
   static const receiptsPageTitle = 'Receipts';
   static const statsPageTitle = 'Statistics';
   static const settingsPageTitle = 'Settings';
+
+  // Titles for the navigation bar.
+  static const ticketsNavTitle = ticketsPageTitle;
+  static const receiptsNavTitle = receiptsPageTitle;
+  static const statsNavTitle = 'Stats';
+  static const settingsNavTitle = settingsPageTitle;
 }
