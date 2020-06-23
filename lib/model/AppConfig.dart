e4bee94eea74b12c48cc38d0ce896a94bc792ef7
Jonas Anker Rasmussen <jonasanker@gmail.com>
2020-06-23 20:14:59 +0200

    Sample RestClient
    RestClient uses Dio, Retrofit and JsonSerializable packages to handle RestApi communication. RestClient and Models are made as abstract classes and the implementation (source code) are generated by running the build script.
    
    See .generate-code.sh to run the build script.

---
+++ lib/model/AppConfig.dart
@@ -0,0 +1,19 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'AppConfig.g.dart';
+
+@JsonSerializable()
+class AppConfig {
+  EnvironmentType environmentType;
+  String merchantId;
+
+  AppConfig(this.environmentType, this.merchantId);
+
+  factory AppConfig.fromJson(Map<String, dynamic> json) => _$AppConfigFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AppConfigToJson(this);
+}
+
+enum EnvironmentType {
+  Production, Test
+}
