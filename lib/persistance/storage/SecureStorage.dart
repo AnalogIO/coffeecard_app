66d6ad72addc31104aa8466b9298f0880a135e69
Jonas Anker Rasmussen <jonasanker@gmail.com>
2020-06-28 00:35:56 +0200

    SecureStorage to read/save token to FlutterSecureStorage
    - SecureStorage class using FlutterSecureStorage
    - Exclude FlutterSecureStorage from android backup to avoid potential issue
    - Bump MinSDKVersion for Android to 18 to support FlutterSecureStorage

---
+++ lib/persistance/storage/SecureStorage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:logger/logger.dart';
+
+class SecureStorage {
+  static const _userTokenKey = "authentication_token";
+
+  final FlutterSecureStorage _storage;
+  final Logger _logger;
+
+  SecureStorage(this._storage, this._logger);
+
+  /// save token to secure storage
+  Future<void> saveToken(String token) async {
+    await _storage.write(key: _userTokenKey, value: token);
+    _logger.d("Token added to Secure Storage");
+  }
+
+  /// read token from secure storage, might be nullable
+  Future<String> readToken() async {
+    return await _storage.read(key: _userTokenKey);
+  }
+}
