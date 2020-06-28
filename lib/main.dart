1a2e566d85c0f5c23b82d3988d30a8c61db3e552
Jonas Anker Rasmussen <jonasanker@gmail.com>
2020-06-28 09:53:26 +0200

    Dependency Injection using GetIt
    Dependencies are configured at startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/material.dart';
-
 import 'package:coffeecard/base/strings.dart';
 import 'package:coffeecard/base/style/theme.dart';
-
+import 'package:coffeecard/service_locator.dart';
 import 'package:coffeecard/widgets/pages/home_page.dart';
+import 'package:flutter/material.dart';
 
 void main() {
+  configureServices();
   runApp(MyApp());
 }
 
