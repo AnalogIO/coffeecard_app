7e2a0c279b45931ec66e5224358cf25a7341b450
Jonas Anker Rasmussen <jonasanker@gmail.com>
2020-06-28 09:52:26 +0200

    Change visibility of Repository implementations to public

--- lib/persistence/repositories/impl/AppConfigRepositoryImpl.dart
+++ lib/persistence/repositories/impl/AppConfigRepositoryImpl.dart
@@ -4,11 +4,11 @@ import 'package:coffeecard/persistence/repositories/AppConfigRepository.dart';
 import 'package:dio/dio.dart';
 import 'package:logger/logger.dart';
 
-class _AppConfigRepository implements AppConfigRepository {
+class AppConfigRepositoryImpl implements AppConfigRepository {
   final RestClient _restClient;
   final Logger _logger;
 
-  _AppConfigRepository(this._restClient, this._logger);
+  AppConfigRepositoryImpl(this._restClient, this._logger);
 
   @override
   Future<AppConfig> getAppConfig() async {
