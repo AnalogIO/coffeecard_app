159ee215000b2f5f5eddf1651542ef7f697ec982
Omid <omidmarfavi@outlook.com>
2021-12-03 10:25:19 +0100

    Add dummy text field in Register page

--- lib/widgets/pages/entry/register_page.dart
+++ lib/widgets/pages/entry/register_page.dart
@@ -1,5 +1,6 @@
 import 'package:coffeecard/base/style/colors.dart';
 import 'package:coffeecard/base/style/text_styles.dart';
+import 'package:coffeecard/widgets/text_field.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatelessWidget {
@@ -12,7 +13,11 @@ class RegisterPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Register', style: AppTextStyle.pageTitle),
       ),
-      body: const Text('bitchass'),
+      body: Column(
+        children: const [
+          AppTextField(autofocus: true),
+        ],
+      ),
     );
   }
 }
