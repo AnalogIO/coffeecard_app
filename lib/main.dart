b141ca829730b3e5af2489c3a362977f82b3e48e
Omid Marfavi <midder@outlook.com>
2021-12-06 22:24:17 +0100

    Adjust navigation stack to behave as expected
    AuthRouter now comes before MaterialApp.
    Now, AuthRouter will never pop off the main nav stack.
    Therefore, AuthRouter can clear the nav stack when switching pages.

--- lib/main.dart
+++ lib/main.dart
@@ -3,36 +3,31 @@ import 'package:coffeecard/base/style/theme.dart';
 import 'package:coffeecard/blocs/authentication/authentication_bloc.dart';
 import 'package:coffeecard/persistence/storage/secure_storage.dart';
 import 'package:coffeecard/service_locator.dart';
+import 'package:coffeecard/widgets/pages/splash_page.dart';
 import 'package:coffeecard/widgets/routers/auth_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   configureServices();
-  runApp(const App());
+  runApp(App());
 }
 
 class App extends StatelessWidget {
-  // TODO How are app instantiated in newest flutter?
-  const App({
-    Key? key,
-  }) : super(key: key);
+  final _navigatorKey = GlobalKey<NavigatorState>();
 
-  @override
-  Widget build(BuildContext context) {
-    return AppView();
-  }
-}
-
-class AppView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => AuthBloc(sl.get<SecureStorage>())..add(AppStarted()),
-      child: MaterialApp(
-        title: Strings.appTitle,
-        theme: analogTheme,
-        home: AuthRouter(),
+      child: AuthRouter(
+        navigatorKey: _navigatorKey,
+        child: MaterialApp(
+          title: Strings.appTitle,
+          theme: analogTheme,
+          home: SplashPage(),
+          navigatorKey: _navigatorKey,
+        ),
       ),
     );
   }
--- lib/widgets/routers/auth_router.dart
+++ lib/widgets/routers/auth_router.dart
@@ -1,6 +1,5 @@
 import 'package:coffeecard/blocs/authentication/authentication_bloc.dart';
 import 'package:coffeecard/widgets/pages/home_page.dart';
-import 'package:coffeecard/widgets/pages/splash_page.dart';
 import 'package:coffeecard/widgets/routers/entry_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -8,23 +7,22 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 // TODO Should prevent going back to the splash page.
 //      (wrapping pages with WillPopScope is one solution)
 class AuthRouter extends StatelessWidget {
+  final GlobalKey<NavigatorState> navigatorKey;
+  final Widget child;
+  const AuthRouter({required this.navigatorKey, required this.child});
+
   @override
   Widget build(BuildContext context) {
     return BlocListener<AuthBloc, AuthState>(
-      // listenWhen: (previous, current) =>
-      //     previous.status != AuthStatus.unknown ||
-      //     current.status != AuthStatus.unknown,
       listener: (context, state) {
-        Navigator.of(context).pushAndRemoveUntil(
+        navigatorKey.currentState!.pushAndRemoveUntil(
           state.status == AuthStatus.authenticated
               ? HomePage.route
               : EntryRouter.route,
-          // TODO Check if route.isFirst is the correct way to handle popUntil.
-          //      (Could possibly break with a stack with >2 routes.)
-          (route) => route.isFirst,
+          (route) => false,
         );
       },
-      child: SplashPage(),
+      child: child,
     );
   }
 }
