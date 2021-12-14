398e2b90fbb1b0c9f8b5fee2d65761f134d9e1ae
Omid <omidmarfavi@outlook.com>
2021-12-14 09:26:16 +0100

    Move log in pages to widgets/pages/entry/login/

--- lib/widgets/pages/entry/login/login_email_page.dart
+++ lib/widgets/pages/entry/login/login_email_page.dart
@@ -1,7 +1,7 @@
 import 'package:coffeecard/widgets/components/entry/login/login_cta.dart';
 import 'package:coffeecard/widgets/components/entry/login/login_email_text_field.dart';
-import 'package:coffeecard/widgets/pages/entry/login_page.dart';
-import 'package:coffeecard/widgets/pages/entry/register_page.dart';
+import 'package:coffeecard/widgets/pages/entry/login/login_page.dart';
+import 'package:coffeecard/widgets/pages/entry/register/register_page.dart';
 import 'package:flutter/material.dart';
 
 void _changeRoute(BuildContext context) {
