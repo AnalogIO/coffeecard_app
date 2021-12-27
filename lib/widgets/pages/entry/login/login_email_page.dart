ea8e8e38019db9152cdca059037a79f75d2be9a8
Omid Marfavi <midder@outlook.com>
2021-12-27 21:48:56 +0100

    Clear login error when navigating to registration

--- lib/widgets/pages/entry/login/login_email_page.dart
+++ lib/widgets/pages/entry/login/login_email_page.dart
@@ -1,10 +1,13 @@
+import 'package:coffeecard/blocs/login/login_bloc.dart';
 import 'package:coffeecard/widgets/components/entry/login/login_cta.dart';
 import 'package:coffeecard/widgets/components/entry/login/login_email_text_field.dart';
 import 'package:coffeecard/widgets/pages/entry/login/login_page.dart';
 import 'package:coffeecard/widgets/pages/entry/register/register_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void _changeRoute(BuildContext context) {
+  BlocProvider.of<LoginBloc>(context).add(const ClearError());
   Navigator.of(context).push(RegisterPage.route);
 }
 
