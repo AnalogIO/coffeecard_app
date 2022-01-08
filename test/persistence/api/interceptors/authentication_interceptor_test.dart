0ac6b9cdf3bbe2cdd90d077d5ef46a0a0881e429
Jonas Anker Rasmussen <jonasanker@gmail.com>
2022-01-09 00:44:37 +0100

    All tests must end with _test.dart

--- test/persistence/api/interceptors/authentication_interceptor_test.dart
+++ test/persistence/api/interceptors/authentication_interceptor_test.dart
@@ -1,16 +1,16 @@
 import 'package:chopper/chopper.dart';
-import 'package:coffeecard/persistence/http/interceptors/authentication_interceptor.dart';
+import 'package:coffeecard/persistence/api/interceptors/authentication_interceptor.dart';
 import 'package:coffeecard/persistence/storage/secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import 'authentication_interceptor_tests.mocks.dart';
+import 'authentication_interceptor_test.mocks.dart';
 
 @GenerateMocks([SecureStorage])
 void main() {
   test(
-      'WHEN calling onRequest GIVEN a token in SecureStorage THEN Authorization Header is added to the request',
+      'GIVEN a token in SecureStorage WHEN calling onRequest THEN Authorization Header is added to the request',
       () async {
     const token =
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
@@ -29,7 +29,7 @@ void main() {
   });
 
   test(
-      'WHEN calling onRequest GIVEN no token in SecureStorage THEN no Authorization Header is added to the request',
+      'GIVEN no token in SecureStorage WHEN calling onRequest THEN no Authorization Header is added to the request',
       () async {
     final mockSecureStorage = MockSecureStorage();
     // ignore: prefer_void_to_null
