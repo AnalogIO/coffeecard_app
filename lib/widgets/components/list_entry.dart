b71331f7056cc6d979c9943545408aaf813cffcb
TTA777 <d6di6skcprs5@opayq.com>
2022-01-11 11:41:48 +0100

    Refactored the receipt page, so the components are now in separate files

---
+++ lib/widgets/components/list_entry.dart
@@ -0,0 +1,45 @@
+import 'package:coffeecard/base/style/colors.dart';
+import 'package:coffeecard/widgets/components/helpers/tappable.dart';
+import 'package:flutter/material.dart';
+
+class ListEntry extends StatelessWidget {
+  final Widget leftWidget;
+  final Widget rightWidget;
+  final void Function()? onTap;
+  final Color? backgroundColor;
+
+  const ListEntry(
+      {required this.leftWidget,
+        required this.rightWidget,
+        this.onTap,
+        this.backgroundColor});
+
+  @override
+  Widget build(BuildContext context) {
+    final childWidget = Container(
+      decoration: BoxDecoration(
+        color: backgroundColor ?? AppColor.white,
+        border: const Border(bottom: BorderSide(color: AppColor.lightGray)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            leftWidget,
+            rightWidget,
+          ],
+        ),
+      ),
+    );
+
+    if (onTap == null) {
+      return childWidget;
+    } else {
+      return Tappable(
+        onTap: onTap!,
+        child: childWidget,
+      );
+    }
+  }
+}
---
+++ lib/widgets/components/receipt/receipt_list_entry.dart
@@ -0,0 +1,48 @@
+import 'package:coffeecard/base/style/colors.dart';
+import 'package:coffeecard/base/style/text_styles.dart';
+import 'package:coffeecard/models/receipts/receipt.dart';
+import 'package:coffeecard/widgets/components/list_entry.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class ReceiptEntry extends StatelessWidget {
+  final Receipt receipt;
+
+  DateFormat get formatter => DateFormat(
+      'dd/MM-yyyy'); //TODO consider if it can be stored centrally, so each entry does not end up with a copy of the formatter
+
+  const ReceiptEntry({
+    required this.receipt,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListEntry(
+      leftWidget: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            receipt.transactionType == TransactionType.purchase
+                ? 'Bought ${receipt.amountPurchased} ${receipt.productName}'
+                : 'Used ${receipt.productName}',
+            style: AppTextStyle.recieptItemKey,
+          ),
+          Text(formatter.format(receipt.timeUsed),
+              style: AppTextStyle.recieptItemDate)
+        ],
+      ),
+      rightWidget: Text(
+        receipt.transactionType == TransactionType.purchase
+            ? '${receipt.price},-'
+            : '${receipt.price} ticket',
+        style: AppTextStyle.recieptItemValue,
+      ),
+      onTap: () {
+        print(receipt);
+      },
+      backgroundColor: receipt.transactionType == TransactionType.purchase
+          ? AppColor.slightlyHighlighted
+          : AppColor.white,
+    );
+  }
+}
