24c79e9546e91eddd9590370e5ca9be44e860966
Jonas Anker Rasmussen <jonasanker@gmail.com>
2022-01-13 10:59:59 +0100

    Update Api to latest changes

--- lib/data/repositories/account_repository.dart
+++ lib/data/repositories/account_repository.dart
@@ -3,8 +3,8 @@ import 'dart:convert';
 import 'package:coffeecard/data/api/coffee_card_api_constants.dart';
 import 'package:coffeecard/generated/api/coffeecard_api.swagger.swagger.dart';
 import 'package:coffeecard/models/account/authenticated_user.dart';
-import 'package:coffeecard/models/account/unauthorized_error.dart';
 import 'package:coffeecard/models/api/api_error.dart';
+import 'package:coffeecard/models/api/unauthorized_error.dart';
 import 'package:crypto/crypto.dart' show sha256;
 import 'package:logger/logger.dart';
 
--- lib/models/account/authenticated_user.dart
+++ lib/models/account/authenticated_user.dart
@@ -1,13 +1,18 @@
+import 'package:equatable/equatable.dart';
+
 /// A user's email and token.
-class AuthenticatedUser {
+class AuthenticatedUser extends Equatable {
   final String email;
   final String token;
 
-  AuthenticatedUser({
+  const AuthenticatedUser({
     required this.email,
     required this.token,
   });
 
   @override
   String toString() => '$email | $token';
+
+  @override
+  List<Object?> get props => [email, token];
 }
