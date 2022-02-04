12944c775649c48f03c2d0e0f76c35daed7b5837
fremartini <frederik.martini@live.dk>
2022-02-04 09:06:32 +0100

    fetch analog open from shifty

--- lib/data/api/coffee_card_api_constants.dart
+++ lib/data/api/coffee_card_api_constants.dart
@@ -2,6 +2,7 @@ class CoffeeCardApiConstants {
   static const String productionUrl = 'https://analogio.dk/clippy';
   static const String testUrl = 'https://beta.analogio.dk/api/clippy';
   static const String betav2Url = 'https://beta-v2.analogio.dk/coffeecard';
+  static const String shiftyUrl = 'https://shifty.analogio.dk/shiftplanning';
 
   static const String apiVersion = '1';
 
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -2,6 +2,7 @@ import 'package:chopper/chopper.dart';
 import 'package:coffeecard/data/api/coffee_card_api_constants.dart';
 import 'package:coffeecard/data/api/interceptors/authentication_interceptor.dart';
 import 'package:coffeecard/data/repositories/receipt_repository.dart';
+import 'package:coffeecard/data/repositories/shiftplanning/opening_hours_repository.dart';
 import 'package:coffeecard/data/repositories/v1/account_repository.dart';
 import 'package:coffeecard/data/repositories/v1/app_config_repository.dart';
 import 'package:coffeecard/data/repositories/v1/coffeecard_repository.dart';
@@ -11,6 +12,8 @@ import 'package:coffeecard/data/repositories/v2/purchase_repository.dart';
 import 'package:coffeecard/data/storage/secure_storage.dart';
 import 'package:coffeecard/generated/api/coffeecard_api.swagger.swagger.dart';
 import 'package:coffeecard/generated/api/coffeecard_api_v2.swagger.swagger.dart'
+    hide $JsonSerializableConverter;
+import 'package:coffeecard/generated/api/shiftplanning_api.swagger.swagger.dart'
     hide $JsonSerializableConverter;
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
@@ -25,18 +28,36 @@ void configureServices() {
   sl.registerSingleton(SecureStorage(sl<Logger>()));
 
   // Rest Client, Chopper client
-  final _chopper = ChopperClient(
+  final _coffeCardChopper = ChopperClient(
     baseUrl: CoffeeCardApiConstants.betav2Url,
     // TODO load the url from config files
     interceptors: [AuthenticationInterceptor(sl<SecureStorage>())],
     converter: $JsonSerializableConverter(),
-    services: [CoffeecardApi.create(), CoffeecardApiV2.create()],
+    services: [
+      CoffeecardApi.create(),
+      CoffeecardApiV2.create(),
+    ],
   );
 
-  sl.registerSingleton<CoffeecardApi>(_chopper.getService<CoffeecardApi>());
-  sl.registerSingleton<CoffeecardApiV2>(_chopper.getService<CoffeecardApiV2>());
+  final _shiftplanningChopper = ChopperClient(
+    baseUrl: CoffeeCardApiConstants.shiftyUrl,
+    // TODO load the url from config files
+    converter: $JsonSerializableConverter(),
+    services: [ShiftplanningApi.create()],
+  );
+
+  sl.registerSingleton<CoffeecardApi>(
+    _coffeCardChopper.getService<CoffeecardApi>(),
+  );
+  sl.registerSingleton<CoffeecardApiV2>(
+    _coffeCardChopper.getService<CoffeecardApiV2>(),
+  );
+  sl.registerSingleton<ShiftplanningApi>(
+    _shiftplanningChopper.getService<ShiftplanningApi>(),
+  );
 
   // Repositories
+  // v1
   sl.registerFactory<AccountRepository>(
     () => AccountRepository(sl<CoffeecardApi>(), sl<Logger>()),
   );
@@ -49,10 +70,6 @@ void configureServices() {
     () => AppConfigRepository(sl<CoffeecardApi>(), sl<Logger>()),
   );
 
-  sl.registerFactory<PurchaseRepository>(
-    () => PurchaseRepository(sl<CoffeecardApiV2>(), sl<Logger>()),
-  );
-
   sl.registerFactory<TicketRepository>(
     () => TicketRepository(sl<CoffeecardApi>(), sl<Logger>()),
   );
@@ -63,5 +80,15 @@ void configureServices() {
 
   sl.registerFactory<ProductRepository>(
     () => ProductRepository(sl<CoffeecardApi>(), sl<Logger>()),
+  );
+
+  // v2
+  sl.registerFactory<PurchaseRepository>(
+    () => PurchaseRepository(sl<CoffeecardApiV2>(), sl<Logger>()),
+  );
+
+  // shiftplanning
+  sl.registerFactory<OpeningHoursRepository>(
+    () => OpeningHoursRepository(sl<ShiftplanningApi>(), sl<Logger>()),
   );
 }
