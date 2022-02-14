a919b19e1738c7f7fc953d3b46c688c41cfba60c
TTA777 <d6di6skcprs5@opayq.com>
2022-02-14 16:20:42 +0100

    Added message to receipt page when no receipts exist. Fixed the ordering of receipts since someone had reversed it. ReceiptListEntry now takes a receipt as argument instead

--- lib/widgets/components/receipt/receipts_list_view.dart
+++ lib/widgets/components/receipt/receipts_list_view.dart
@@ -1,4 +1,6 @@
+import 'package:coffeecard/base/strings.dart';
 import 'package:coffeecard/base/style/colors.dart';
+import 'package:coffeecard/base/style/text_styles.dart';
 import 'package:coffeecard/cubits/receipt/receipt_cubit.dart';
 import 'package:coffeecard/models/receipts/receipt.dart';
 import 'package:coffeecard/widgets/components/receipt/receipt_list_entry.dart';
@@ -19,25 +21,40 @@ class ReceiptsListView extends StatelessWidget {
             return RefreshIndicator(
               displacement: 24,
               onRefresh: context.read<ReceiptCubit>().fetchReceipts,
-              child: ListView.builder(
-                itemCount: state.filteredReceipts.length,
-                itemBuilder: (_, index) {
-                  final r = state.filteredReceipts[index];
-                  if (r.transactionType == TransactionType.purchase) {
-                    return ReceiptListEntry.purchase(
-                      productName: r.productName,
-                      time: r.timeUsed,
-                      quantity: r.amountPurchased,
-                      price: r.price,
-                    );
-                  } else {
-                    return ReceiptListEntry.swipe(
-                      productName: r.productName,
-                      time: r.timeUsed,
-                    );
-                  }
-                },
-              ),
+              child: state.filteredReceipts.isEmpty
+                  ? Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Container(
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                width: 2,
+                                color: AppColor.secondary,
+                              ),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                'Receipts for your purchases and swipes, will show up here',
+                                style: AppTextStyle.explainer,
+                                overflow: TextOverflow.visible,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  : ListView.builder(
+                      itemCount: state.filteredReceipts.length,
+                      itemBuilder: (_, index) {
+                        final r = state.filteredReceipts[index];
+                        return ReceiptListEntry(receipt: r);
+                      },
+                    ),
             );
           },
         ),
@@ -47,8 +64,20 @@ class ReceiptsListView extends StatelessWidget {
 }
 
 class _ReceiptsPlaceholder extends StatelessWidget {
-  final placeholderListEntries =
-      List.generate(20, (_) => ReceiptListEntry.placeholder());
+  final placeholderListEntries = List.generate(
+    20,
+    (_) => ReceiptListEntry(
+      receipt: Receipt(
+        timeUsed: DateTime.now(),
+        productName: Strings.receiptPlaceholderName,
+        transactionType: TransactionType.placeholder,
+        price: -1,
+        amountPurchased: 1,
+        id: -1,
+      ),
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return ListView(
