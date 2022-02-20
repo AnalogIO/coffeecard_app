6fed1ab982434b7954aef0c951905dbd4ce422e6
Jens-Fastrup <jenssarrofastrup@gmail.com>
2022-02-20 14:33:13 +0100

    delete redudancy

--- lib/base/style/text_styles.dart
+++ lib/base/style/text_styles.dart
@@ -134,22 +134,10 @@ abstract class AppTextStyle {
     fontWeight: FontWeight.w500,
     fontFamily: AppFont.body,
   );
-
   static final TextStyle overLine = TextStyle(
     color: AppColor.primary,
     fontSize: 12,
     fontFamily: AppFont.body,
-  );
-
-  static final TextStyle newLabel = TextStyle(
-    fontSize: 12,
-    letterSpacing: 1,
-    color: AppColor.ticket,
-    fontWeight: FontWeight.w500,
-    fontFamily: AppFont.body,
-  );
-  static final TextStyle comingSoonLabel = newLabel.copyWith(
-    color: AppColor.gray,
   );
   static final TextStyle label = TextStyle(
     color: AppColor.secondary,
