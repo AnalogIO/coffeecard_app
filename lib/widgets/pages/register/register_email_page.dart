9d17b4d0f21d1537342ecedb079abf11b7f2d9d6
fremartini <frederik.martini@live.dk>
2022-02-23 17:01:45 +0100

    add continue buttons to register flow pages, remove splash effect on rounded_button when disabled

--- lib/widgets/pages/register/register_email_page.dart
+++ lib/widgets/pages/register/register_email_page.dart
@@ -1,11 +1,11 @@
 import 'package:coffeecard/base/strings.dart';
-import 'package:coffeecard/widgets/components/entry/register/register_email_text_field.dart';
+import 'package:coffeecard/widgets/components/entry/register/register_email_body.dart';
 import 'package:coffeecard/widgets/pages/register/register_page.dart';
 
 class RegisterEmailPage extends RegisterPage {
   const RegisterEmailPage()
       : super(
           sectionTitle: Strings.registerEmailTitle,
-          body: const RegisterEmailTextField(),
+          body: const RegisterEmailBody(),
         );
 }
