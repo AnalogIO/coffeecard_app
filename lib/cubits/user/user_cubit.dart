12f4b24b15d7469367adfa213e106d0ca10d365e
Omid Marfavi <midder@outlook.com>
2022-03-07 11:47:07 +0100

    wip

--- lib/cubits/user/user_cubit.dart
+++ lib/cubits/user/user_cubit.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:coffeecard/data/repositories/v1/account_repository.dart';
-import 'package:coffeecard/generated/api/coffeecard_api.swagger.models.swagger.dart';
 import 'package:coffeecard/models/account/user.dart';
 import 'package:equatable/equatable.dart';
 
@@ -24,10 +23,10 @@ class UserCubit extends Cubit<UserState> {
   }
 
   Future<void> setUserPrivacy({required bool privacyActived}) async {
-    emit(UserLoading());
+    emit(UserUpdating());
 
-    final either = await accountRepository
-        .updateUser(UpdateUserDto(privacyActivated: privacyActived));
+    final either =
+        await accountRepository.updatePrivacy(private: privacyActived);
 
     if (either.isRight) {
       emit(UserLoaded(either.right));
