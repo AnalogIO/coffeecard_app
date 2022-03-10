b12f0a93dfbb54777ac72fbaf1a0721017d8c3b0
Thomas Andersen <d6di6skcprs5@opayq.com>
2022-03-10 19:10:43 +0100

    Users can swipe tickets (#182)
    When a user swipes a ticket, it now uses a ticket in the backend, and displays a receipt

--- lib/widgets/components/tickets_section.dart
+++ lib/widgets/components/tickets_section.dart
@@ -4,6 +4,8 @@ import 'package:coffeecard/cubits/tickets_page/tickets_cubit.dart';
 import 'package:coffeecard/errors/match_case_incomplete_exception.dart';
 import 'package:coffeecard/service_locator.dart';
 import 'package:coffeecard/widgets/components/coffee_card.dart';
+import 'package:coffeecard/widgets/components/loading_overlay.dart';
+import 'package:coffeecard/widgets/components/receipt/receipt_overlay.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -26,13 +28,23 @@ class TicketSection extends StatelessWidget {
               ),
             ),
             BlocConsumer<TicketsCubit, TicketsState>(
-              listener: (context, state) {},
+              listener: (context, state) {
+                if (state is TicketUsing) {
+                  Navigator.pop(context); //Removes the swipe overlay
+                  LoadingOverlay.of(context)
+                      .show(); //TODO consider using a nicer loading indicator
+                } else if (state is TicketUsed) {
+                  Navigator.pop(context); //Removes the loading overlay
+                  ReceiptOverlay.of(context).show(state.receipt);
+                }
+              },
               builder: (context, state) {
                 if (state is TicketsLoading) {
                   final ticketsCubit = context.read<TicketsCubit>();
                   ticketsCubit.getTickets();
                   return const Text('loading');
                 } else if (state is TicketsLoaded) {
+                  //States extending this are also caught on this
                   return Column(
                     children: state.tickets.isEmpty
                         ? [const EmptyCoffeeCard()]
@@ -41,6 +53,7 @@ class TicketSection extends StatelessWidget {
                               (p) => CoffeeCard(
                                 title: p.productName,
                                 amount: p.count,
+                                productId: p.productId,
                               ),
                             )
                             .toList(),
