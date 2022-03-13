a3ebd745ae9f2e91bb588d949fa250fdc8190352
fremartini <frederik.martini@live.dk>
2022-03-13 13:31:48 +0100

    re-add programme name

--- lib/widgets/pages/settings/your_profile_page.dart
+++ lib/widgets/pages/settings/your_profile_page.dart
@@ -1,8 +1,8 @@
 import 'package:coffeecard/base/strings.dart';
 import 'package:coffeecard/base/style/text_styles.dart';
 import 'package:coffeecard/cubits/user/user_cubit.dart';
+import 'package:coffeecard/errors/match_case_incomplete_exception.dart';
 import 'package:coffeecard/models/account/user.dart';
-import 'package:coffeecard/utils/responsive.dart';
 import 'package:coffeecard/widgets/components/loading.dart';
 import 'package:coffeecard/widgets/components/scaffold.dart';
 import 'package:coffeecard/widgets/components/settings_group.dart';
@@ -48,56 +48,58 @@ class _EditProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Gap(24),
-        const CircleAvatar(radius: 54),
-        const Gap(12),
-        Text(
-          user.name,
-          style: AppTextStyle.sectionTitle,
-          textAlign: TextAlign.center,
-        ),
-        const Gap(8),
-        //FIXME: lookup on programme id
-        Text('${user.programmeId}', style: AppTextStyle.explainer),
-        const Gap(24),
-        SettingsGroup(
-          title: Strings.settingsGroupProfile,
-          description: Strings.yourProfileDescription,
-          listItems: [
-            SettingListEntry(
-              name: Strings.name,
-              valueWidget: SettingDescription(text: user.name),
-              onTap: () {},
-            ),
-            SettingListEntry(
-              name: Strings.occupation,
-              //FIXME: lookup on programme id
-              valueWidget: SettingDescription(text: '${user.programmeId}'),
-              onTap: () {},
-            ),
-            SettingListEntry(
-              name: Strings.changeProfilePicture,
-              valueWidget: const SettingDescription(),
-              onTap: () {},
-            ),
-            SettingListEntry(
-              name: deviceIsSmall(context)
-                  ? Strings.appearAnonymousSmall
-                  : Strings.appearAnonymous,
-              valueWidget: Switch(
-                value: user.privacyActivated,
-                onChanged: (privacyActived) async {
-                  await context
-                      .read<UserCubit>()
-                      .setUserPrivacy(privacyActived: privacyActived);
-                },
+    return BlocBuilder<UserCubit, UserState>(
+      builder: (context, state) {
+        if (state is UserLoading) {
+          return const CircularProgressIndicator();
+        } else if (state is UserError) {
+          //FIXME: display error page?
+          return const SizedBox.shrink();
+        } else if (state is UserLoaded) {
+          return Column(
+            children: [
+              const Gap(24),
+              const CircleAvatar(radius: 54),
+              const Gap(12),
+              Text(
+                user.name,
+                style: AppTextStyle.sectionTitle,
+                textAlign: TextAlign.center,
               ),
-            ),
-          ],
-        ),
-      ],
+              const Gap(8),
+              Text(
+                '${state.user.programme.fullName} (${state.user.programme.shortName})',
+                style: AppTextStyle.explainer,
+              ),
+              const Gap(24),
+              SettingsGroup(
+                title: Strings.settingsGroupProfile,
+                listItems: [
+                  SettingListEntry(
+                    name: Strings.name,
+                    valueWidget: SettingDescription(text: user.name),
+                    onTap: () {},
+                  ),
+                  SettingListEntry(
+                    name: Strings.occupation,
+                    valueWidget: SettingDescription(
+                      text: state.user.programme.shortName,
+                    ),
+                    onTap: () {},
+                  ),
+                  SettingListEntry(
+                    name: Strings.changeProfilePicture,
+                    valueWidget: const SettingDescription(),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ],
+          );
+        }
+
+        throw MatchCaseIncompleteException(this);
+      },
     );
   }
 }
