713745fb432f579d3fb250f99a6df89dfa2be6aa
TTA777 <d6di6skcprs5@opayq.com>
2022-03-18 13:06:45 +0100

    Added support for changing a users programme

--- lib/widgets/pages/settings/your_profile_page.dart
+++ lib/widgets/pages/settings/your_profile_page.dart
@@ -5,6 +5,7 @@ import 'package:coffeecard/models/account/user.dart';
 import 'package:coffeecard/utils/page_pusher.dart';
 import 'package:coffeecard/utils/responsive.dart';
 import 'package:coffeecard/widgets/components/entry/register/name_body.dart';
+import 'package:coffeecard/widgets/components/list_entry.dart';
 import 'package:coffeecard/widgets/components/loading.dart';
 import 'package:coffeecard/widgets/components/scaffold.dart';
 import 'package:coffeecard/widgets/components/settings_group.dart';
@@ -26,8 +27,7 @@ class YourProfilePage extends StatelessWidget {
           if (userLoadedState is! UserLoaded) return const SizedBox.shrink();
 
           return BlocBuilder<UserCubit, UserState>(
-            buildWhen: (previous, current) =>
-                previous is UserUpdating || current is UserUpdating,
+            buildWhen: (previous, current) => previous is UserUpdating || current is UserUpdating,
             builder: (context, state) {
               return Loading(
                 loading: state is UserUpdating,
@@ -46,6 +46,7 @@ class YourProfilePage extends StatelessWidget {
 
 class _EditProfile extends StatelessWidget {
   const _EditProfile({required this.user});
+
   final User user;
 
   @override
@@ -79,14 +80,11 @@ class _EditProfile extends StatelessWidget {
               valueWidget: SettingDescription(
                 text: user.programme.shortName,
               ),
+              onTap: () => _pushChangeProgrammePage(context),
             ),
             SettingListEntry(
-              name: deviceIsSmall(context)
-                  ? Strings.appearAnonymousSmall
-                  : Strings.appearAnonymous,
-              onTap: () => context
-                  .read<UserCubit>()
-                  .setUserPrivacy(privacyActivated: !user.privacyActivated),
+              name: deviceIsSmall(context) ? Strings.appearAnonymousSmall : Strings.appearAnonymous,
+              onTap: () => context.read<UserCubit>().setUserPrivacy(privacyActivated: !user.privacyActivated),
               valueWidget: Switch(
                 value: user.privacyActivated,
                 onChanged: (_) {},
@@ -107,6 +105,40 @@ class _EditProfile extends StatelessWidget {
         onSubmit: (context, name) {
           context.read<UserCubit>().setUserName(name);
           Navigator.pop(context);
+        },
+      ),
+    );
+  }
+
+  Future<void> _pushChangeProgrammePage(BuildContext context) {
+    return pushPageScaffold(
+      context: context,
+      title: Strings.changeProgramme,
+      body: BlocBuilder<UserCubit, UserState>(
+        builder: (context, state) {
+          if (state is UserLoaded) {
+            return ListView.builder(
+              itemCount: state.programmes.length,
+              itemBuilder: (context, index) {
+                state.programmes.sort((a, b) => a.fullName!.compareTo(b.fullName!));
+                final programme = state.programmes[index];
+                return ListEntry(
+                  leftWidget: SizedBox(
+                    //TODO Is there a better way to determine the width of the left widget
+                    width: MediaQuery.of(context).size.width * (3 / 5),
+                    child: Text(programme.fullName!),
+                  ),
+                  rightWidget: Text(programme.shortName!),
+                  onTap: () {
+                    context.read<UserCubit>().setUserProgramme(programme.id!);
+                    Navigator.pop(context);
+                  },
+                );
+              },
+            );
+          }
+          //TODO handle programmes not being loaded?
+          return const Text('Error');
         },
       ),
     );
