54c2d599ee4e9e91e3200f934df0f60108cd3981
Omid Marfavi <21163286+marfavi@users.noreply.github.com>
2022-04-01 21:15:17 +0200

    Fix #239 (Navigating on login/logout fails) (#240)
    * Fix #239 (Navigating on login/logout fails)
    Splash router needs to sit above the MaterialApp widget, as it would otherwise get removed from the navigation stack.
    
    * Remove FIXME in env cubit

--- lib/widgets/routers/splash_router.dart
+++ lib/widgets/routers/splash_router.dart
@@ -1,14 +1,15 @@
 import 'package:coffeecard/cubits/authentication/authentication_cubit.dart';
 import 'package:coffeecard/cubits/environment/environment_cubit.dart';
 import 'package:coffeecard/widgets/pages/home_page.dart';
-import 'package:coffeecard/widgets/pages/splash/splash_error_page.dart';
-import 'package:coffeecard/widgets/pages/splash/splash_loading_page.dart';
-import 'package:coffeecard/widgets/routers/entry_router.dart';
+import 'package:coffeecard/widgets/routers/login_router.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SplashRouter extends StatefulWidget {
-  const SplashRouter();
+  const SplashRouter({required this.navigatorKey, required this.child});
+
+  final GlobalKey<NavigatorState> navigatorKey;
+  final Widget child;
 
   @override
   _SplashRouterState createState() => _SplashRouterState();
@@ -22,10 +23,10 @@ class _SplashRouterState extends State<SplashRouter> {
     final authStatus = context.read<AuthenticationCubit>().state.status;
 
     if (authStatus.isUnknown || envState is! EnvironmentLoaded) return;
+
     // FIXME: The transition needs animation
-    Navigator.pushAndRemoveUntil(
-      context,
-      authStatus.isAuthenticated ? HomePage.route : EntryRouter.route,
+    widget.navigatorKey.currentState!.pushAndRemoveUntil(
+      authStatus.isAuthenticated ? HomePage.route : LoginRouter.route,
       (route) => false,
     );
   }
@@ -41,15 +42,7 @@ class _SplashRouterState extends State<SplashRouter> {
           listener: (context, _) => _maybeNavigate(context),
         ),
       ],
-      child: BlocBuilder<EnvironmentCubit, EnvironmentState>(
-        builder: (context, state) {
-          if (state is EnvironmentLoaded || state is EnvironmentInitial) {
-            return const SplashLoadingPage();
-          } else {
-            return const SplashErrorPage();
-          }
-        },
-      ),
+      child: widget.child,
     );
   }
 }
