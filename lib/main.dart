bc2b8cfbb3ddfb2b0cd829806e2face13cef7c3e
Omid Marfavi <midder@outlook.com>
2022-05-09 13:52:58 +0200

    Force screen orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:coffeecard/widgets/pages/splash/splash_error_page.dart';
 import 'package:coffeecard/widgets/pages/splash/splash_loading_page.dart';
 import 'package:coffeecard/widgets/routers/splash_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
@@ -25,6 +26,9 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Force screen orientation to portrait
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
     return MultiBlocProvider(
       providers: [
         BlocProvider.value(value: sl<AuthenticationCubit>()..appStarted()),
