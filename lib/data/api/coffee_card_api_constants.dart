4bfcdf78b9d07c067062b31fa8547f4d190727b3
Jonas Anker Rasmussen <jonasanker@gmail.com>
2022-05-20 00:40:04 +0200

    Change baseUrl to beta.analogio.dk API v2 url (#300)

--- lib/data/api/coffee_card_api_constants.dart
+++ lib/data/api/coffee_card_api_constants.dart
@@ -1,7 +1,6 @@
 class CoffeeCardApiConstants {
   static const String productionUrl = 'https://analogio.dk/clippy';
   static const String testUrl = 'https://beta.analogio.dk/api/clippy';
-  static const String betav2Url = 'https://api-test.analogio.dk/coffeecard';
   static const String shiftyUrl = 'https://shifty.analogio.dk/shiftplanning';
 
   static const String apiVersion = '1';
