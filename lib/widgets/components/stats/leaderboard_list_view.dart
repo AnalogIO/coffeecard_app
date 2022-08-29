a14788198501981737a452857fa2f13d6f40f2e5
fremartini <frederik.martini@live.dk>
2022-08-29 11:42:34 +0200

    user is no longer shown on leaderboard when not on top X

--- lib/widgets/components/stats/leaderboard_list_view.dart
+++ lib/widgets/components/stats/leaderboard_list_view.dart
@@ -1,7 +1,6 @@
 import 'package:coffeecard/cubits/statistics/statistics_cubit.dart';
 import 'package:coffeecard/cubits/user/user_cubit.dart';
 import 'package:coffeecard/models/account/user.dart';
-import 'package:coffeecard/models/leaderboard_user.dart';
 import 'package:coffeecard/widgets/components/stats/leaderboard_entry.dart';
 import 'package:flutter/material.dart';
 
@@ -18,11 +17,6 @@ class LeaderboardListView extends StatelessWidget {
   Widget build(BuildContext context) {
     final leaderboard = statsState.leaderboard;
     final userRank = _getUserRank(userState.user, statsState.filterBy);
-    final userInLeaderboard = userRank <= leaderboard.length;
-    if (!userInLeaderboard) {
-      // TODO: how can we access an individual's score by id?
-      leaderboard.add(LeaderboardUser(name: userState.user.name, score: 0));
-    }
 
     return ListView.builder(
       shrinkWrap: true,
@@ -30,12 +24,11 @@ class LeaderboardListView extends StatelessWidget {
       itemCount: leaderboard.length,
       itemBuilder: (context, index) {
         final entry = leaderboard[index];
-        final isUsersEntry = index == userRank - 1;
         return LeaderboardEntry(
           name: entry.name,
           score: entry.score,
           rank: index + 1,
-          highlight: isUsersEntry,
+          highlight: index == userRank - 1,
         );
       },
     );
@@ -43,8 +36,12 @@ class LeaderboardListView extends StatelessWidget {
 }
 
 int _getUserRank(User user, StatisticsFilterCategory filterBy) {
-  if (filterBy == StatisticsFilterCategory.month) return user.rankMonth;
-  if (filterBy == StatisticsFilterCategory.semester) return user.rankSemester;
-  if (filterBy == StatisticsFilterCategory.total) return user.rankTotal;
-  throw Exception('Unknown filter category: $filterBy');
+  switch (filterBy) {
+    case StatisticsFilterCategory.month:
+      return user.rankMonth;
+    case StatisticsFilterCategory.semester:
+      return user.rankSemester;
+    case StatisticsFilterCategory.total:
+      return user.rankTotal;
+  }
 }
