d4be0ae285894bcb83e09e6ffe43c161c495d219
Omid Marfavi <21163286+marfavi@users.noreply.github.com>
2022-09-06 17:06:45 +0200

    Add TicketsCubit tests

--- lib/cubits/tickets/tickets_cubit.dart
+++ lib/cubits/tickets/tickets_cubit.dart
@@ -1,16 +1,15 @@
 import 'package:coffeecard/data/repositories/v1/ticket_repository.dart';
 import 'package:coffeecard/models/receipts/receipt.dart';
 import 'package:coffeecard/models/ticket/ticket_count.dart';
-import 'package:coffeecard/utils/either.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'tickets_state.dart';
 
 class TicketsCubit extends Cubit<TicketsState> {
-  final TicketRepository _ticketRepository;
-
   TicketsCubit(this._ticketRepository) : super(const TicketsLoading());
+
+  final TicketRepository _ticketRepository;
 
   Future<void> getTickets() async {
     emit(const TicketsLoading());
@@ -18,26 +17,26 @@ class TicketsCubit extends Cubit<TicketsState> {
   }
 
   Future<void> useTicket(int productId) async {
-    if (state is TicketsLoaded) {
-      final previousState = state as TicketsLoaded;
-      emit(TicketUsing(previousState.tickets));
-      final response = await _ticketRepository.useTicket(productId);
-      if (response is Right) {
-        emit(TicketUsed(response.right, previousState.tickets));
-        // Refresh tickets, so the user sees the right count
+    final st = state;
+    if (st is! TicketsLoaded) return;
+
+    emit(TicketUsing(st.tickets));
+    final either = await _ticketRepository.useTicket(productId);
+
+    either.fold(
+      (error) => emit(TicketsError(error.message)),
+      (receipt) {
+        emit(TicketUsed(receipt, st.tickets));
         refreshTickets();
-      } else {
-        emit(TicketsError(response.left.message));
-      }
-    }
+      },
+    );
   }
 
   Future<void> refreshTickets() async {
-    final response = await _ticketRepository.getUserTickets();
-    if (response is Right) {
-      emit(TicketsLoaded(response.right));
-    } else {
-      emit(TicketsError(response.left.message));
-    }
+    final either = await _ticketRepository.getUserTickets();
+    either.fold(
+      (error) => emit(TicketsError(error.message)),
+      (tickets) => emit(TicketsLoaded(tickets)),
+    );
   }
 }
