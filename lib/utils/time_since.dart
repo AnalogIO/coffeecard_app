11a7fde60220af469a76b1701603fa5edec603c5
fremartini <frederik.martini@live.dk>
2022-11-08 18:02:20 +0100

    scaffold

--- lib/utils/time_since.dart
+++ lib/utils/time_since.dart
@@ -1,21 +1,6 @@
 import 'package:coffeecard/base/strings.dart';
 
 String timeSince(DateTime time) {
-  String monthsAgo(int days) {
-    final months = days ~/ 30.5;
-    final monthsStr =
-        months == 1 ? Strings.aMonth : '$months ${Strings.months}';
-    return '${Strings.around} $monthsStr ${Strings.ago}';
-  }
-
-  String yearsAgo(int days) {
-    final years = days ~/ 365;
-    final rest = days % 365;
-    if (rest > 305) return '${Strings.almost} ${years + 1} ${Strings.yearsAgo}';
-    if (rest < 60) return '${Strings.around} $years ${Strings.yearsAgo}';
-    return '${Strings.moreThan} $years ${Strings.yearsAgo}';
-  }
-
   final currentTime = DateTime.now();
   final diff = currentTime.difference(time);
 
@@ -28,4 +13,18 @@ String timeSince(DateTime time) {
   if (diff.inDays < 31) return '${diff.inDays} ${Strings.daysAgo}';
   if (diff.inDays < 365) return monthsAgo(diff.inDays);
   return yearsAgo(diff.inDays);
+}
+
+String monthsAgo(int days) {
+  final months = days ~/ 30.5;
+  final monthsStr = months == 1 ? Strings.aMonth : '$months ${Strings.months}';
+  return '${Strings.around} $monthsStr ${Strings.ago}';
+}
+
+String yearsAgo(int days) {
+  final years = days ~/ 365;
+  final rest = days % 365;
+  if (rest > 305) return '${Strings.almost} ${years + 1} ${Strings.yearsAgo}';
+  if (rest < 60) return '${Strings.around} $years ${Strings.yearsAgo}';
+  return '${Strings.moreThan} $years ${Strings.yearsAgo}';
 }
---
+++ test/utils/time_since_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('time since tests', () {
+    test('years ago given XYZ does XYZ', () {
+      expect('', '');
+    });
+    test('', () {
+      expect('', '');
+    });
+  });
+}
