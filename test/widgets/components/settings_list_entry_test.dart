c37968d80d373274268d596eb9af003a48c6d807
Frederik Petersen <fred.p@live.dk>
2022-11-15 18:09:05 +0100

    add ui tests for leaderboard list entry

--- test/widgets/components/settings_list_entry_test.dart
+++ test/widgets/components/settings_list_entry_test.dart
@@ -4,7 +4,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('SettingListEntry should match golden file', (tester) async {
+  testWidgets('SettingsListEntry should match golden file with "normal" value',
+      (tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: SettingListEntry(
+            name: 'email',
+            valueWidget: SettingValueText(
+              value: 'Normal email',
+            ),
+          ),
+        ),
+      ),
+    );
+
+    expect(find.byType(SettingListEntry), findsOneWidget);
+    expect(find.text('Normal email'), findsOneWidget);
+
+    await expectLater(
+      find.byType(SettingListEntry),
+      matchesGoldenFile('goldens/settings_list_entry_normal.png'),
+    );
+  });
+  testWidgets('SettingListEntry when truncated should match golden file',
+      (tester) async {
     await tester.pumpWidget(
       const MaterialApp(
         home: Scaffold(
@@ -20,7 +44,7 @@ void main() {
     );
     await expectLater(
       find.byType(SettingListEntry),
-      matchesGoldenFile('goldens/setting_list_entry.png'),
+      matchesGoldenFile('goldens/setting_list_entry_truncated.png'),
     );
   });
 }
