4e61136dd598cc570aa0851e3a0a6da37d62f588
Frederik Martini <39860858+fremartini@users.noreply.github.com>
2022-11-22 20:07:28 +0100

    Test and improve time since utility (#369)
    Co-authored-by: Omid Marfavi <21163286+marfavi@users.noreply.github.com>

--- lib/utils/time_since.dart
+++ lib/utils/time_since.dart
@@ -1,22 +1,12 @@
 import 'package:coffeecard/base/strings.dart';
 
 String timeSince(DateTime time) {
-  String monthsAgo(int days) {
-    final months = days ~/ 30.5;
-    final monthsStr =
-        months == 1 ? Strings.aMonth : '$months ${Strings.months}';
-    return '${Strings.around} $monthsStr ${Strings.ago}';
-  }
-
-  String yearsAgo(int days) {
-    final years = days ~/ 365;
-    final rest = days % 365;
-    if (rest > 305) return '${Strings.almost} ${years + 1} ${Strings.yearsAgo}';
-    if (rest < 60) return '${Strings.around} $years ${Strings.yearsAgo}';
-    return '${Strings.moreThan} $years ${Strings.yearsAgo}';
-  }
-
   final currentTime = DateTime.now();
+
+  if (time.isAfter(currentTime)) {
+    return Strings.inTheFuture;
+  }
+
   final diff = currentTime.difference(time);
 
   if (diff.inMinutes < 2) return Strings.justNow;
@@ -26,6 +16,28 @@ String timeSince(DateTime time) {
   if (diff.inDays == 0) return Strings.earlierToday;
   if (diff.inDays == 1) return Strings.yesterday;
   if (diff.inDays < 31) return '${diff.inDays} ${Strings.daysAgo}';
-  if (diff.inDays < 365) return monthsAgo(diff.inDays);
-  return yearsAgo(diff.inDays);
+  if (diff.inDays < 365) return monthsAgo(days: diff.inDays);
+  return yearsAgo(days: diff.inDays);
+}
+
+String monthsAgo({required int days}) {
+  final months = days ~/ 30.5;
+  final rest = days % 30.5;
+  if (rest > 28) return Strings.monthsAgo(months + 1);
+  if (rest < 3) return Strings.monthsAgo(months);
+
+  if (rest > 23) return '${Strings.almost} ${Strings.monthsAgo(months + 1)}';
+  if (rest < 7) return '${Strings.around} ${Strings.monthsAgo(months)}';
+  return '${Strings.moreThan} ${Strings.monthsAgo(months)}';
+}
+
+String yearsAgo({required int days}) {
+  final years = days ~/ 365;
+  final rest = days % 365;
+  if (rest > 355) return Strings.yearsAgo(years + 1);
+  if (rest < 10) return Strings.yearsAgo(years);
+
+  if (rest > 305) return '${Strings.almost} ${Strings.yearsAgo(years + 1)}';
+  if (rest < 60) return '${Strings.around} ${Strings.yearsAgo(years)}';
+  return '${Strings.moreThan} ${Strings.yearsAgo(years)}';
 }
