9b1ddfad73a0b580d95b49405f6d59702f1edf5e
Omid Marfavi <21163286+marfavi@users.noreply.github.com>
2023-02-14 15:05:51 +0100

    Add barrel file

---
+++ lib/features/opening_hours/opening_hours.dart
@@ -0,0 +1,7 @@
+export 'data/datasources/opening_hours_remote_data_source.dart';
+export 'data/repositories/opening_hours_repository_impl.dart';
+export 'domain/repositories/opening_hours_repository.dart';
+export 'domain/usecases/fetch_opening_hours.dart';
+export 'domain/usecases/is_open.dart';
+export 'presentation/cubit/opening_hours_cubit.dart';
+export 'presentation/widgets/opening_hours_indicator.dart';
