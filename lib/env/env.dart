2ebd51b83c2869a91b3b592c618d09a5433d16b6
fremartini <frederik.martini@live.dk>
2023-03-04 15:18:05 +0100

    use environment specific .env files

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -2,7 +2,7 @@ import 'package:envied/envied.dart';
 
 part 'env.g.dart';
 
-@Envied(path: '.env')
+@Envied(path: '.env.develop')
 abstract class Env {
   @EnviedField(varName: 'coffeeCardUrl')
   static const coffeeCardUrl = _Env.coffeeCardUrl;
