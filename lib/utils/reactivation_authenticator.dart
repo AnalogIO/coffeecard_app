380df7478bbef31fdd86f61863bfe06fe59084b3
Thomas Andersen <d6di6skcprs5@opayq.com>
2023-03-28 22:13:51 +0200

    Fixed the authenticator not returning the updated request

--- lib/utils/reactivation_authenticator.dart
+++ lib/utils/reactivation_authenticator.dart
@@ -29,17 +29,18 @@ class ReactivationAuthenticator extends Authenticator {
       tokenRefreshedAt == null ||
       DateTime.now().difference(tokenRefreshedAt!) > debounce;
 
-  Future<void> _evict() async => authenticationCubit.unauthenticated();
+  Future<void> _evict() => authenticationCubit.unauthenticated();
 
   Map<String, String> _updateHeadersWithToken(
     Map<String, String> headers,
     String token,
   ) {
-    headers.update(
+    final _ = headers.update(
       'Authorization',
       (String _) => token,
       ifAbsent: () => token,
     );
+
     return headers;
   }
 
@@ -49,13 +50,16 @@ class ReactivationAuthenticator extends Authenticator {
     Response response, [
     Request? originalRequest,
   ]) async {
-    logger.d(
-      '${request.url} ${response.statusCode}\n${response.bodyString}',
-    );
     if (response.statusCode != 401) {
+      logger.d(
+        '${request.method} ${request.url} ${response.statusCode}',
+      );
+
       return null;
     }
-
+    logger.d(
+      '${request.method} ${request.url} ${response.statusCode}\n${response.bodyString}',
+    );
     if (mutex.isLocked()) {
       // someone is updating the token, wait until they are done and read it
       await mutex.wait();
@@ -73,7 +77,7 @@ class ReactivationAuthenticator extends Authenticator {
 
     // avoid refreshing the token multiple times if requests happen at the same time
     if (_canRefreshToken()) {
-      return await refreshToken(request, response);
+      return await refreshToken(request);
     }
 
     return null;
@@ -81,24 +85,29 @@ class ReactivationAuthenticator extends Authenticator {
 
   Future<Request?> refreshToken(
     Request request,
-    Response response,
   ) async {
     final email = await secureStorage.readEmail();
     final encodedPasscode = await secureStorage.readEncodedPasscode();
 
-    if (email != null && encodedPasscode != null) {
+    if (email == null || encodedPasscode == null) {
+      //User is not logged in
+      return null;
+    }
+    mutex.lock();
+    try {
       final accountRepository = serviceLocator.get<AccountRepository>();
 
-      mutex.lock();
-
       // this call may return 401 which triggers a recursive call, use a guard
-      try {
-        final either = await accountRepository.login(email, encodedPasscode);
+      final either = await accountRepository.login(email, encodedPasscode);
 
-        either.fold((l) {
+      return either.fold(
+        (l) {
           // refresh failed, sign the user out
           _evict();
-        }, (r) async {
+
+          return null;
+        },
+        (r) async {
           // refresh succeeded, update the token in secure storage
           tokenRefreshedAt = DateTime.now();
 
@@ -109,11 +118,10 @@ class ReactivationAuthenticator extends Authenticator {
           return request.copyWith(
             headers: _updateHeadersWithToken(request.headers, bearerToken),
           );
-        });
-      } finally {
-        mutex.unlock();
-      }
+        },
+      );
+    } finally {
+      mutex.unlock();
     }
-    return null;
   }
 }
