e1261dfd085fbe06e5d3d3e9e878cad0c8a328da
fremartini <frederik.martini@live.dk>
2023-04-16 10:24:37 +0200

    add getters to role

--- lib/features/user/domain/entities/role.dart
+++ lib/features/user/domain/entities/role.dart
@@ -2,10 +2,15 @@ import 'package:coffeecard/features/user/domain/entities/roles.dart';
 import 'package:equatable/equatable.dart';
 
 class Role extends Equatable {
-  final Roles userGroup;
+  final Roles role;
 
-  const Role(this.userGroup);
+  const Role(this.role);
+
+  bool get isBarista => role == Roles.barista;
+  bool get isManager => role == Roles.manager;
+  bool get isBoard => role == Roles.board;
+  bool get isCustomer => role == Roles.customer;
 
   @override
-  List<Object?> get props => [userGroup];
+  List<Object?> get props => [role];
 }
