5f6a8b4c42cf7a8c0edb40c5eb3d09f914078fa8
Thomas Andersen <d6di6skcprs5@opayq.com>
2023-04-25 21:08:19 +0200

    Made cubits factories instead of singletons

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -188,7 +188,7 @@ void initOpeningHours() {
 
 void initTicket() {
   // bloc
-  sl.registerLazySingleton(
+  sl.registerFactory(
     () => TicketsCubit(loadTickets: sl(), consumeTicket: sl()),
   );
 }
@@ -245,7 +245,7 @@ void initUser() {
 
 void initReceipt() {
   // bloc
-  sl.registerLazySingleton(() => ReceiptCubit(getReceipts: sl()));
+  sl.registerFactory(() => ReceiptCubit(getReceipts: sl()));
 
   // use case
   sl.registerFactory(() => GetReceipts(repository: sl()));
