dd3270d18f5b527431ec025bc00a0b592332d074
Omid Marfavi <21163286+marfavi@users.noreply.github.com>
2023-05-13 11:37:29 +0200

    Auto fix some issues

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
-    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+    show TargetPlatform, defaultTargetPlatform, kIsWeb;
 
 abstract class DefaultFirebaseOptions {
   static FirebaseOptions get production {
