145515ccd2348a76fa35ba5382dff334d8e878ec
Omid Marfavi <21163286+marfavi@users.noreply.github.com>
2023-05-15 09:39:18 +0200

    Add interface to classes

--- lib/core/usecases/usecase.dart
+++ lib/core/usecases/usecase.dart
@@ -2,7 +2,7 @@ import 'package:coffeecard/core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
-abstract class UseCase<Type, Params> {
+abstract interface class UseCase<Type, Params> {
   Future<Either<Failure, Type>> call(Params params);
 }
 
--- lib/features/receipt/domain/repositories/receipt_repository.dart
+++ lib/features/receipt/domain/repositories/receipt_repository.dart
@@ -2,6 +2,6 @@ import 'package:coffeecard/core/errors/failures.dart';
 import 'package:coffeecard/features/receipt/domain/entities/receipt.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class ReceiptRepository {
+abstract interface class ReceiptRepository {
   Future<Either<Failure, List<Receipt>>> getUserReceipts();
 }
