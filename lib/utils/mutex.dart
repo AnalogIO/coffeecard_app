d106b46f3055690603918626756c6dedee9ab0d5
Omid Marfavi <21163286+marfavi@users.noreply.github.com>
2023-05-23 23:48:41 +0200

    Improve Mutex class?

--- lib/utils/mutex.dart
+++ lib/utils/mutex.dart
@@ -1,15 +1,16 @@
 import 'dart:async';
 
+/// A mutex that allows only one asynchronous operation to run at a time.
 class Mutex {
-  Completer _lock = Completer<void>();
+  Completer<void> _lock = Completer<void>();
 
-  /// Returns whether the lock is acquired.
+  /// Returns whether the lock is currently acquired.
   bool get isLocked => !_lock.isCompleted;
 
-  /// Wait until the lock is released, then run the given action (without
+  /// Waits until the lock is released, then runs the given action (without
   /// acquiring the lock).
   ///
-  /// If the lock is not acquired, the action will be run immediately.
+  /// If the lock is not currently acquired, the action will be run immediately.
   Future<T> runWithoutLock<T>(FutureOr<T> Function() action) async {
     if (isLocked) {
       await _lock.future;
@@ -19,35 +20,16 @@ class Mutex {
 
   /// Acquires the lock, runs the given action, and then releases the lock.
   ///
-  /// If the lock is already acquired, this call will wait until the lock is
-  /// released.
+  /// If the lock is already acquired, this method will wait until the lock is
+  /// released before acquiring it.
   Future<T> run<T>(FutureOr<T> Function() action) async {
-    await _acquire();
+    await _lock.future;
     try {
       return await action();
     } finally {
-      _release();
+      if (_lock.isCompleted) {
+        _lock = Completer<void>();
+      }
     }
-  }
-
-  /// Acquires the lock.
-  ///
-  /// If the lock is already acquired, this call will wait until the lock is
-  /// released.
-  Future<void> _acquire() async {
-    if (_lock.isCompleted) {
-      _lock = Completer<void>();
-    }
-    await _lock.future;
-  }
-
-  /// Releases the lock.
-  ///
-  /// If the lock is not acquired, this call will throw an error.
-  void _release() {
-    if (!_lock.isCompleted) {
-      throw StateError('Cannot release a lock that is not acquired.');
-    }
-    _lock.complete();
   }
 }
