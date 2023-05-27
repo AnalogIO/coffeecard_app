b884a006525acd6ef7d3e7d5db0fb767037c5f8e
fremartini <frederik.martini@live.dk>
2023-05-27 09:32:22 +0200

    move forms

--- lib/features/settings/presentation/pages/change_email_page.dart
+++ lib/features/settings/presentation/pages/change_email_page.dart
@@ -1,8 +1,8 @@
 import 'package:coffeecard/base/strings.dart';
 import 'package:coffeecard/cubits/authentication/authentication_cubit.dart';
+import 'package:coffeecard/features/settings/presentation/widgets/forms/change_email_form.dart';
 import 'package:coffeecard/features/user/presentation/cubit/user_cubit.dart';
 import 'package:coffeecard/widgets/components/dialog.dart';
-import 'package:coffeecard/widgets/components/forms/settings/change_email_form.dart';
 import 'package:coffeecard/widgets/components/scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
--- lib/features/settings/presentation/widgets/change_passcode_flow.dart
+++ lib/features/settings/presentation/widgets/change_passcode_flow.dart
@@ -1,5 +1,5 @@
 import 'package:coffeecard/base/strings.dart';
-import 'package:coffeecard/widgets/components/forms/settings/change_passcode_form.dart';
+import 'package:coffeecard/features/settings/presentation/widgets/forms/change_passcode_form.dart';
 import 'package:coffeecard/widgets/components/scaffold.dart';
 import 'package:coffeecard/widgets/routers/app_flow.dart';
 import 'package:flutter/material.dart';
