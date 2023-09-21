0ce2202da6ab673d879227599c08f39e7ba844c6
fremartini <frederik.martini@live.dk>
2023-09-21 18:01:25 +0200

    cubit --> bloc

--- lib/features/form/presentation/widgets/form.dart
+++ lib/features/form/presentation/widgets/form.dart
@@ -3,7 +3,7 @@ import 'package:coffeecard/core/styles/app_colors.dart';
 import 'package:coffeecard/core/validator/input_validator.dart';
 import 'package:coffeecard/core/widgets/components/section_title.dart';
 import 'package:coffeecard/core/widgets/components/tickets/rounded_button.dart';
-import 'package:coffeecard/features/form/presentation/cubit/form_bloc.dart';
+import 'package:coffeecard/features/form/presentation/bloc/form_bloc.dart';
 import 'package:flutter/material.dart' hide FormState;
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
