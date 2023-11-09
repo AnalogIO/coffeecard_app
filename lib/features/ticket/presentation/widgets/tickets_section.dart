c316f6c7a2d816fa77f40f5928ae141ee8c14ded
Omid Marfavi <21163286+marfavi@users.noreply.github.com>
2023-11-09 19:37:06 +0100

    remove uneccesary blocbuilder in tickets section

--- lib/features/ticket/presentation/widgets/tickets_section.dart
+++ lib/features/ticket/presentation/widgets/tickets_section.dart
@@ -23,122 +23,116 @@ class TicketSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TicketsCubit, TicketsState>(
-      builder: (context, state) {
-        return Column(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SectionTitle(Strings.ticketsMyTickets),
-                OpeningHoursIndicator(),
-              ],
-            ),
-            BlocConsumer<TicketsCubit, TicketsState>(
-              listenWhen: (previous, current) =>
-                  current is TicketUsing ||
-                  current is TicketUsed ||
-                  current is TicketsUseError,
-              listener: (context, state) {
-                if (state is TicketUsing) {
-                  if (Navigator.of(context, rootNavigator: true).canPop()) {
-                    // If a ticket was used from the buy
-                    // single drink flow, there is no swipe overlay
-
-                    // Remove the swipe overlay
-                    Navigator.of(context, rootNavigator: true).pop();
-                  }
-
-                  LoadingOverlay.show(context).ignore();
-                }
-                if (state is TicketUsed) {
-                  // Refresh or load user info (for updated rank stats)
-                  // (also refreshes leaderboard)
-                  context.read<UserCubit>().fetchUserDetails();
-
-                  final envState = context.read<EnvironmentCubit>().state;
-                  LoadingOverlay.hide(context);
-                  ReceiptOverlay.show(
-                    productName: state.receipt.productName,
-                    timeUsed: state.receipt.timeUsed,
-                    isTestEnvironment:
-                        envState is EnvironmentLoaded && envState.env.isTest,
-                    status: state.receipt is PurchaseReceipt
-                        ? (state.receipt as PurchaseReceipt)
-                            .paymentStatus
-                            .toString()
-                        : Strings.swiped,
-                    context: context,
-                  ).ignore();
-                }
-                if (state is TicketsUseError) {
-                  LoadingOverlay.hide(context);
-                  appDialog(
-                    context: context,
-                    title: Strings.error,
-                    actions: [
-                      TextButton(
-                        child: const Text(Strings.buttonOK),
-                        onPressed: () => closeAppDialog(context),
-                      ),
-                    ],
-                    children: [
-                      Text(state.message, style: AppTextStyle.settingKey),
-                    ],
-                    dismissible: true,
-                  );
-                }
-              },
-              buildWhen: (previous, current) =>
-                  current is TicketsLoading ||
-                  current is TicketsLoaded ||
-                  current is TicketsLoadError,
-              builder: (context, state) {
-                if (state is TicketsLoading) {
-                  return const _CoffeeCardLoading();
-                }
-                if (state is TicketsLoaded) {
-                  // States extending this are also caught on this
-                  if (state.tickets.isEmpty) {
-                    return const Padding(
-                      padding: EdgeInsets.only(bottom: 12.0),
-                      child: CoffeeCardPlaceholder(),
-                    );
-                  }
-                  return Column(
-                    children: state.tickets
-                        .map(
-                          (p) => Padding(
-                            padding: const EdgeInsets.only(bottom: 12.0),
-                            child: Hero(
-                              tag: (p.productName, p.productId),
-                              child: CoffeeCard(
-                                title: p.productName,
-                                amountOwned: p.count,
-                                productId: p.productId,
-                              ),
-                            ),
-                          ),
-                        )
-                        .toList(),
-                  );
-                }
-                if (state is TicketsLoadError) {
-                  return ErrorSection(
-                    error: state.message,
-                    retry: context.read<TicketsCubit>().getTickets,
-                  );
-                }
-
-                throw ArgumentError(this);
-              },
-            ),
-            const Gap(4),
+            SectionTitle(Strings.ticketsMyTickets),
+            OpeningHoursIndicator(),
           ],
-        );
-      },
+        ),
+        BlocConsumer<TicketsCubit, TicketsState>(
+          listenWhen: (previous, current) =>
+              current is TicketUsing ||
+              current is TicketUsed ||
+              current is TicketsUseError,
+          listener: (context, state) {
+            if (state is TicketUsing) {
+              if (Navigator.of(context, rootNavigator: true).canPop()) {
+                // If a ticket was used from the buy
+                // single drink flow, there is no swipe overlay
+
+                // Remove the swipe overlay
+                Navigator.of(context, rootNavigator: true).pop();
+              }
+
+              LoadingOverlay.show(context).ignore();
+            }
+            if (state is TicketUsed) {
+              // Refresh or load user info (for updated rank stats)
+              // (also refreshes leaderboard)
+              context.read<UserCubit>().fetchUserDetails();
+
+              final envState = context.read<EnvironmentCubit>().state;
+              LoadingOverlay.hide(context);
+              ReceiptOverlay.show(
+                productName: state.receipt.productName,
+                timeUsed: state.receipt.timeUsed,
+                isTestEnvironment:
+                    envState is EnvironmentLoaded && envState.env.isTest,
+                status: state.receipt is PurchaseReceipt
+                    ? (state.receipt as PurchaseReceipt)
+                        .paymentStatus
+                        .toString()
+                    : Strings.swiped,
+                context: context,
+              ).ignore();
+            }
+            if (state is TicketsUseError) {
+              LoadingOverlay.hide(context);
+              appDialog(
+                context: context,
+                title: Strings.error,
+                actions: [
+                  TextButton(
+                    child: const Text(Strings.buttonOK),
+                    onPressed: () => closeAppDialog(context),
+                  ),
+                ],
+                children: [Text(state.message, style: AppTextStyle.settingKey)],
+                dismissible: true,
+              );
+            }
+          },
+          buildWhen: (previous, current) =>
+              current is TicketsLoading ||
+              current is TicketsLoaded ||
+              current is TicketsLoadError,
+          builder: (context, state) {
+            if (state is TicketsLoading) {
+              return const _CoffeeCardLoading();
+            }
+            if (state is TicketsLoaded) {
+              // States extending this are also caught on this
+              if (state.tickets.isEmpty) {
+                return const Padding(
+                  padding: EdgeInsets.only(bottom: 12.0),
+                  child: CoffeeCardPlaceholder(),
+                );
+              }
+              return Column(
+                children: state.tickets
+                    .map(
+                      (p) => Padding(
+                        padding: const EdgeInsets.only(bottom: 12.0),
+                        child: Hero(
+                          tag: (p.productName, p.productId),
+                          child: CoffeeCard(
+                            title: p.productName,
+                            amountOwned: p.count,
+                            productId: p.productId,
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              );
+            }
+            if (state is TicketsLoadError) {
+              return ErrorSection(
+                error: state.message,
+                retry: context.read<TicketsCubit>().getTickets,
+              );
+            }
+
+            throw ArgumentError(this);
+          },
+        ),
+        const Gap(4),
+      ],
     );
   }
 }
