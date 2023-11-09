0d5c0e5c94f04dca3dd1e12e03456a7eec926ab6
Omid Marfavi <21163286+marfavi@users.noreply.github.com>
2023-11-09 18:53:22 +0100

    rename BuyProductsPage file

--- lib/features/product/presentation/pages/buy_tickets_page.dart
+++ lib/features/product/presentation/pages/buy_tickets_page.dart
@@ -1,6 +1,6 @@
 import 'package:coffeecard/core/strings.dart';
 import 'package:coffeecard/features/product/domain/entities/purchasable_products.dart';
-import 'package:coffeecard/features/product/presentation/pages/buy_page.dart';
+import 'package:coffeecard/features/product/presentation/pages/buy_products_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
