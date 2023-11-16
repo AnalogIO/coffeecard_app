0ee23439f1ec5ebd86b4f1654da21d2acab4ac18
Frederik Martini <39860858+fremartini@users.noreply.github.com>
2023-11-16 20:38:39 +0100

    refactor: opening hours (#541)
    Fix opening hours to match the cafe
    
    Uses the flutter-native TimeOfDay class, which has localized time formatting.
    This has been extended with comparison methods <= and isInTimeslot.
    Timeslot class simplifies with this change.
    
    Simplify DateService to only contain one getter.
    
    Make sure of fpdart's Option instead of null values. Use None for closed days.
    
    Clean up tests, add more tests, and use NiceMocks for mocking.
    ---------
    
    Co-authored-by: Omid Marfavi <21163286+marfavi@users.noreply.github.com>

--- lib/features/opening_hours/data/repositories/opening_hours_repository_impl.dart
+++ lib/features/opening_hours/data/repositories/opening_hours_repository_impl.dart
@@ -1,7 +1,10 @@
 import 'package:coffeecard/core/external/date_service.dart';
 import 'package:coffeecard/features/opening_hours/data/datasources/opening_hours_local_data_source.dart';
 import 'package:coffeecard/features/opening_hours/domain/entities/opening_hours.dart';
+import 'package:coffeecard/features/opening_hours/domain/entities/timeslot.dart';
 import 'package:coffeecard/features/opening_hours/domain/repositories/opening_hours_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:fpdart/fpdart.dart';
 
 class OpeningHoursRepositoryImpl implements OpeningHoursRepository {
   final OpeningHoursLocalDataSource dataSource;
@@ -15,9 +18,11 @@ class OpeningHoursRepositoryImpl implements OpeningHoursRepository {
   @override
   OpeningHours getOpeningHours() {
     final allOpeningHours = dataSource.getOpeningHours();
-    final currentWeekDay = dateService.currentWeekday();
+    final currentWeekday = dateService.currentDateTime.weekday;
 
-    final todaysOpeningHours = allOpeningHours[currentWeekDay]!;
+    final todaysOpeningHours = Option.fromNullable(
+      allOpeningHours[currentWeekday],
+    );
 
     return OpeningHours(
       allOpeningHours: allOpeningHours,
@@ -28,16 +33,12 @@ class OpeningHoursRepositoryImpl implements OpeningHoursRepository {
   @override
   bool isOpen() {
     final todaysOpeningHours = getOpeningHours().todaysOpeningHours;
+    final currentTimeOfDay =
+        TimeOfDay.fromDateTime(dateService.currentDateTime);
 
-    if (todaysOpeningHours.isClosed) {
-      return false;
-    }
-
-    final currentHour = dateService.currentHour();
-
-    if (currentHour < todaysOpeningHours.start! ||
-        currentHour > todaysOpeningHours.end!) return false;
-
-    return true;
+    return todaysOpeningHours.match(
+      () => false,
+      currentTimeOfDay.isInTimeslot,
+    );
   }
 }
