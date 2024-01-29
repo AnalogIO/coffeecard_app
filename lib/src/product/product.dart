fdf35f84427b2e0e46ed378f488078acc34e3986
Omid Marfavi <21163286+marfavi@users.noreply.github.com>
2024-01-30 00:20:59 +0100

    move cubit into bloc folder

--- lib/src/product/product.dart
+++ lib/src/product/product.dart
@@ -1,8 +1,8 @@
+export 'bloc/product_cubit.dart';
 export 'functions.dart';
 export 'models/menu_item_model.dart';
 export 'models/product_model.dart';
 export 'models/purchasable_products_model.dart';
-export 'product_cubit.dart';
 export 'product_repository.dart';
 export 'widgets/buy_products_page.dart';
 export 'widgets/buy_single_drink_page.dart';
