14a809a44630f70c21237e49fa0f3ba218c6a72f
Omid Marfavi <21163286+marfavi@users.noreply.github.com>
2024-11-05 20:43:22 +0100

    simplify upgrade alert

--- lib/features/login/presentation/pages/login_page_base.dart
+++ lib/features/login/presentation/pages/login_page_base.dart
@@ -2,10 +2,10 @@ import 'package:coffeecard/core/styles/app_text_styles.dart';
 import 'package:coffeecard/core/widgets/components/helpers/responsive.dart';
 import 'package:coffeecard/core/widgets/components/scaffold.dart';
 import 'package:coffeecard/core/widgets/images/analog_logo.dart';
-import 'package:coffeecard/core/widgets/upgrader.dart';
 import 'package:coffeecard/features/login/presentation/widgets/login_input_hint.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:upgrader/upgrader.dart';
 
 class LoginPageBase extends StatelessWidget {
   const LoginPageBase({
@@ -28,7 +28,7 @@ class LoginPageBase extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Upgrader(
+    return UpgradeAlert(
       child: AppScaffold.withoutTitle(
         resizeToAvoidBottomInset: resizeToAvoidBottomInset,
         body: Column(
