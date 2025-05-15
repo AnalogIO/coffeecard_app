de46297beab1b0bcf86504dbcf7a46c2000010da
Omid Marfavi <21163286+marfavi@users.noreply.github.com>
2025-05-15 17:42:40 +0200

    fix(purchase): hide perks products in "buy single drink" page (#591)
    Attempting to claim perks in the "Buy single drink" page currently
    causes an error. Since these perks should only be shown on the home page
    anyways, they have been removed from the "Buy single drink" page.

--- lib/features/product/presentation/cubit/product_state.dart
+++ lib/features/product/presentation/cubit/product_state.dart
@@ -17,7 +17,7 @@ class ProductsLoaded extends ProductState {
   ProductsLoaded(Iterable<Product> products)
       : products = (
           clipCards: products.where((p) => p.amount > 1),
-          singleDrinks: products.where((p) => p.amount == 1),
+          singleDrinks: products.where((p) => p.amount == 1 && !p.isPerk),
           perks: products.where((p) => p.isPerk),
         );
 
